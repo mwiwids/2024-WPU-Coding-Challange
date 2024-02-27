775812450dd724b1b9eab4fed9c4a52fe3af2400
mwiwids <m.wiwid.s@gmail.com>
2024-02-28 06:22:17 +0700

    Fix codewars link

--- 009-FindMaximumAndMinimum.js
+++ 009-FindMaximumAndMinimum.js
@@ -1,5 +1,5 @@
 // WPU Coding Challange day 9/366
-// www.codewars.com/kata/5b077ebdaf15be5c7f000077
+// www.codewars.com/kata/577a98a6ae28071780000989
 
 // Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language )
 // that receive a list of integers as input, and return the largest and lowest number in that list, respectively.

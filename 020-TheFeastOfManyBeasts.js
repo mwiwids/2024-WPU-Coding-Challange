67260a2e659be104caff8c81327c07fccc418dbe
mwiwids <m.wiwid.s@gmail.com>
2024-03-12 22:36:15 +0700

    day 20/366

---
+++ 020-TheFeastOfManyBeasts.js
@@ -0,0 +1,28 @@
+// WPU Coding Challange day 20/366
+// www.codewars.com/kata/5aa736a455f906981800360d
+// Keywords : Fundamental, String
+
+// All of the animals are having a feast! Each animal is bringing one dish.
+// There is just one rule: the dish must start and end with the same letters as the animal's name.
+// For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
+
+// Write a function feast that takes the animal's name and dish as arguments
+// and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
+
+// Assume that beast and dish are always lowercase strings, and that each has at least two letters.
+// beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string.
+// They will not contain numerals.
+
+/*
+function feast(beast, dish) {
+  return (
+    beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
+  );
+}
+*/
+
+const feast = (beast, dish) =>
+  beast[0] === dish[0] && beast.at(-1) === dish.at(-1);
+
+console.log(feast("great blue heron", "garlic naan"));
+console.log(feast("brown bear", "bear claw"));

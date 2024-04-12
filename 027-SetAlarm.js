41eb24f9b8614b6d9652a2eaa051c8c203c4d59f
mwiwids <m.wiwid.s@gmail.com>
2024-04-12 15:29:34 +0700

    day 27/366

---
+++ 027-SetAlarm.js
@@ -0,0 +1,30 @@
+// WPU Coding Challange day 27/366
+// www.codewars.com/kata/568dcc3c7f12767a62000038
+// Keywords : Fundamental, Logic
+
+// Write a function named setAlarm which receives two parameters.
+// The first parameter, employed, is true whenever you are employed
+// and the second parameter, vacation is true whenever you are on vacation.
+
+// The function should return true if you are employed and not on vacation
+// (because these are the circumstances under which you need to set an alarm).
+// It should return false otherwise. Examples:
+
+// employed | vacation
+// true     | true     => false
+// true     | false    => true
+// false    | true     => false
+// false    | false    => false
+
+// function setAlarm(employed, vacation) {
+//   if (employed && !vacation) return true;
+//   else return false;
+// }
+
+// function setAlarm(employed, vacation) {
+//   return employed && !vacation;
+// }
+
+const setAlarm = (employed, vacation) => employed && !vacation;
+
+console.log(setAlarm(true, true));

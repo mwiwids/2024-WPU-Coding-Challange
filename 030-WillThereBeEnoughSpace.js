76758f3286c2adf65e8c5a0729191a581a680c0b
mwiwids <m.wiwid.s@gmail.com>
2024-04-14 12:50:38 +0700

    use max

--- 030-WillThereBeEnoughSpace.js
+++ 030-WillThereBeEnoughSpace.js
@@ -22,14 +22,20 @@
 // cap = 10, on = 5, wait = 5 --> 0 # He can fit all 5 passengers
 // cap = 100, on = 60, wait = 50 --> 10 # He can't fit 10 of the 50 waiting
 
-// function enough(cap, on, wait) {
-//   if (cap >= on + wait) return 0;
-//   else return on + wait - cap;
-// }
+/*
+function enough(cap, on, wait) {
+  if (cap >= on + wait) return 0;
+  else return on + wait - cap;
+}
 
-// function enough(cap, on, wait) {
-//   return cap >= on + wait ? 0 : on + wait - cap;
-// }
+function enough(cap, on, wait) {
+  return cap >= on + wait ? 0 : on + wait - cap;
+}
+
+function enough(cap, on, wait) {
+  return Math.max(on + wait - cap, 0);
+}
+*/
 
 const enough = (cap, on, wait) => (cap >= on + wait ? 0 : on + wait - cap);
 

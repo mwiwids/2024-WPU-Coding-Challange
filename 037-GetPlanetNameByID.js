6b04eb2ef7a028bf58c64e5d79401505f5ebf22c
mwiwids <m.wiwid.s@gmail.com>
2024-04-15 22:35:23 +0700

    day 37/366

---
+++ 037-GetPlanetNameByID.js
@@ -0,0 +1,90 @@
+// WPU Coding Challange day 37/366
+// www.codewars.com/kata/515e188a311df01cba000003
+// Keywords : Debugging
+
+// The function is not returning the correct values. Can you figure out why?
+
+// Example (Input --> Output ):
+// 3 --> "Earth"
+
+/*
+function getPlanetName(id) {
+  var name;
+  switch (id) {
+    case 1:
+      name = "Mercury";
+      break;
+    case 2:
+      name = "Venus";
+      break;
+    case 3:
+      name = "Earth";
+      break;
+    case 4:
+      name = "Mars";
+      break;
+    case 5:
+      name = "Jupiter";
+      break;
+    case 6:
+      name = "Saturn";
+      break;
+    case 7:
+      name = "Uranus";
+      break;
+    case 8:
+      name = "Neptune";
+      break;
+  }
+
+  return name;
+}
+
+function getPlanetName(id) {
+  switch (id) {
+    case 1:
+      return "Mercury";
+    case 2:
+      return "Venus";
+    case 3:
+      return "Earth";
+    case 4:
+      return "Mars";
+    case 5:
+      return "Jupiter";
+    case 6:
+      return "Saturn";
+    case 7:
+      return "Uranus";
+    case 8:
+      return "Neptune";
+  }
+}
+
+function getPlanetName(id) {
+  return {
+    1: "Mercury",
+    2: "Venus",
+    3: "Earth",
+    4: "Mars",
+    5: "Jupiter",
+    6: "Saturn",
+    7: "Uranus",
+    8: "Neptune",
+  }[id];
+}
+*/
+
+const getPlanetName = (id) =>
+  ({
+    1: "Mercury",
+    2: "Venus",
+    3: "Earth",
+    4: "Mars",
+    5: "Jupiter",
+    6: "Saturn",
+    7: "Uranus",
+    8: "Neptune",
+  }[id]);
+
+console.log(getPlanetName(3));
